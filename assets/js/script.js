e1634c4d0ed98de08620bb18083784663158d6b2
MtrQ <130695828+SplendorSA@users.noreply.github.com>
2023-10-17 08:19:09 -0500

    a

---
+++ assets/js/script.js
@@ -0,0 +1,178 @@
+const imageClassPrefix = 'bg-image';
+const section = document.getElementById('Parara');
+let currentImageIndex = 0;
+const images = [
+    'path-to-image1.jpg',
+    'path-to-image2.jpg',
+    'path-to-image3.jpg',
+    'path-to-image4.jpg'
+];
+
+function changeBackground(imageElement, className) {
+    const images = imageElement.parentNode.parentNode.querySelectorAll('img');
+    images.forEach(img => img.classList.remove('selected'));
+    imageElement.classList.add('selected');
+
+    section.classList.remove(getCurrentBackgroundClass());
+    section.classList.add(className);
+    currentImageIndex = Array.from(images).indexOf(imageElement);
+}
+
+function getCurrentBackgroundClass() {
+    const classList = section.classList;
+    for (let i = 0; i < classList.length; i++) {
+        const className = classList[i];
+        if (className.startsWith(imageClassPrefix)) {
+            return className;
+        }
+    }
+    return 'bg-prueba';
+}
+
+function changeBackgroundAutomatically() {
+    currentImageIndex++;
+    if (currentImageIndex >= images.length) {
+        currentImageIndex = 0;
+    }
+
+    const imageElements = section.querySelectorAll('.image-list li img');
+    const imageElement = imageElements[currentImageIndex];
+    const className = 'bg-image' + (currentImageIndex + 1);
+    changeBackground(imageElement, className);
+}
+
+setInterval(changeBackgroundAutomatically, 5000);
+
+
+
+function NumeroCali1() {
+    /* Texto que deseas copiar al portapapeles */
+    var textoACopiar = "(+57) 312 259 8401";
+    
+    /* Crea un elemento de texto temporal */
+    var tempInput = document.createElement("input");
+    tempInput.value = textoACopiar;
+    
+    /* Agrega el elemento al DOM */
+    document.body.appendChild(tempInput);
+    
+    /* Selecciona el contenido del elemento de texto */
+    tempInput.select();
+    tempInput.setSelectionRange(0, 99999); /* Para dispositivos móviles */
+    
+    /* Copia el contenido al portapapeles */
+    document.execCommand("copy");
+    
+    /* Remueve el elemento temporal */
+    document.body.removeChild(tempInput);
+}
+
+function NumeroCali2() {
+    /* Texto que deseas copiar al portapapeles */
+    var textoACopiar = "(+57) 310 892 7784";
+    
+    /* Crea un elemento de texto temporal */
+    var tempInput = document.createElement("input");
+    tempInput.value = textoACopiar;
+    
+    /* Agrega el elemento al DOM */
+    document.body.appendChild(tempInput);
+    
+    /* Selecciona el contenido del elemento de texto */
+    tempInput.select();
+    tempInput.setSelectionRange(0, 99999); /* Para dispositivos móviles */
+    
+    /* Copia el contenido al portapapeles */
+    document.execCommand("copy");
+    
+    /* Remueve el elemento temporal */
+    document.body.removeChild(tempInput);
+}
+
+function CorreoCali() {
+    /* Texto que deseas copiar al portapapeles */
+    var textoACopiar = "comercial@splendorpublicidad.com";
+    
+    /* Crea un elemento de texto temporal */
+    var tempInput = document.createElement("input");
+    tempInput.value = textoACopiar;
+    
+    /* Agrega el elemento al DOM */
+    document.body.appendChild(tempInput);
+    
+    /* Selecciona el contenido del elemento de texto */
+    tempInput.select();
+    tempInput.setSelectionRange(0, 99999); /* Para dispositivos móviles */
+    
+    /* Copia el contenido al portapapeles */
+    document.execCommand("copy");
+    
+    /* Remueve el elemento temporal */
+    document.body.removeChild(tempInput);
+}
+
+function NumeroBogota1() {
+    /* Texto que deseas copiar al portapapeles */
+    var textoACopiar = "(+57) 321 829 4656";
+    
+    /* Crea un elemento de texto temporal */
+    var tempInput = document.createElement("input");
+    tempInput.value = textoACopiar;
+    
+    /* Agrega el elemento al DOM */
+    document.body.appendChild(tempInput);
+    
+    /* Selecciona el contenido del elemento de texto */
+    tempInput.select();
+    tempInput.setSelectionRange(0, 99999); /* Para dispositivos móviles */
+    
+    /* Copia el contenido al portapapeles */
+    document.execCommand("copy");
+    
+    /* Remueve el elemento temporal */
+    document.body.removeChild(tempInput);
+}
+
+function NumeroBogota2() {
+    /* Texto que deseas copiar al portapapeles */
+    var textoACopiar = "(+57) 320 727 1651";
+    
+    /* Crea un elemento de texto temporal */
+    var tempInput = document.createElement("input");
+    tempInput.value = textoACopiar;
+    
+    /* Agrega el elemento al DOM */
+    document.body.appendChild(tempInput);
+    
+    /* Selecciona el contenido del elemento de texto */
+    tempInput.select();
+    tempInput.setSelectionRange(0, 99999); /* Para dispositivos móviles */
+    
+    /* Copia el contenido al portapapeles */
+    document.execCommand("copy");
+    
+    /* Remueve el elemento temporal */
+    document.body.removeChild(tempInput);
+}
+
+function CorreoBogota() {
+    /* Texto que deseas copiar al portapapeles */
+    var textoACopiar = "comercial4@splendorpublicidad.com";
+    
+    /* Crea un elemento de texto temporal */
+    var tempInput = document.createElement("input");
+    tempInput.value = textoACopiar;
+    
+    /* Agrega el elemento al DOM */
+    document.body.appendChild(tempInput);
+    
+    /* Selecciona el contenido del elemento de texto */
+    tempInput.select();
+    tempInput.setSelectionRange(0, 99999); /* Para dispositivos móviles */
+    
+    /* Copia el contenido al portapapeles */
+    document.execCommand("copy");
+    
+    /* Remueve el elemento temporal */
+    document.body.removeChild(tempInput);
+}
